dcbb12c8b275896bc7fb007755233208e4e80167
Jaiden2013YT <jkjaiden2013@gmail.com>
2026-03-20 15:02:35 -0700

    Update popcorn.js

--- popcorn.js
+++ popcorn.js
@@ -3,3 +3,12 @@ this is the js file for the popcorn thing!
 */
 
 const popcornDisplay = document.getElementById("popcornDisplay")
+
+function popcornLoop(p) {
+
+  p.setup = function() {
+    p.createCanvas(400, 700 popcornDisplay)
+  }
+  
+  
+}
