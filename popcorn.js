d7fd003a4880da2af1d9c82df8fe6902a81568b3
Jaiden2013YT <jkjaiden2013@gmail.com>
2026-03-20 21:14:18 -0700

    Update popcorn.js

--- popcorn.js
+++ popcorn.js
@@ -11,7 +11,7 @@ function popcornLoop(p) {
     canvas.parent(popcornDisplay)
     canvas.style.touchAction = "none"
 
-    let box = new sprite()
+    let box = new p.sprite()
   }
   
   p.draw = function () {
