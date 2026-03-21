356c9673a8a6d33274d2f57dcaca067659a5208b
Jaiden2013YT <jkjaiden2013@gmail.com>
2026-03-21 15:49:10 -0700

    Update popcorn.js

--- popcorn.js
+++ popcorn.js
@@ -24,7 +24,7 @@ window.draggingElement = -1
     let rightWall  = new p.Sprite(p.width+(wallWidth/2), p.height/2, wallWidth, p.height, p.STATIC) // off screen
     //let cool       = new p.Sprite(30, 30, 30, 30, p.STATIC)
     let cookerOriginX = 100
-    let cookerOriginY = p.width-100
+    let cookerOriginY = 300
 
     let cookerRight = new p.Sprite((cookerOriginX-50)+wallWidth, cookerOriginY, wallWidth, 75, p.STATIC)
     //let cool = new p.Sprite(65, 300, 15, 75, p.STATIC)
